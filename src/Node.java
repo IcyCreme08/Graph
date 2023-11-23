b9547bf2af45f5d4f18c6e54bd6dfb22c04a4543
IcyCreme <63565085+IcyCreme@users.noreply.github.com>
2023-11-23 11:32:37 -0500

    node

--- src/Node.java
+++ src/Node.java
@@ -1,10 +1,18 @@
 import java.util.ArrayList;
 import java.util.Arrays;
 
-public class Node<T> {
+public class Node<T> implements Comparable<Node> {
     private ArrayList<Node<T>> connections = new ArrayList<>();
+    private T data;
     public Node(T data, Node<T>... nodes){
         this.connections.addAll(Arrays.asList(nodes));
     }
-    public Node()
+    public Node(T data){
+        this.data = data;
+    }
+
+    public T data(){
+        return this.data;
+    }
 }
+public class Node<>
